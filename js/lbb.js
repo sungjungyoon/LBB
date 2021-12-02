c4a934cb3a79d9e95305db780f53855d73798f0f
sungjungyoon <87048278+sungjungyoon@users.noreply.github.com>
2021-12-02 22:31:38 +0900

    Add files via upload

--- js/lbb.js
+++ js/lbb.js
@@ -1,4 +1,12 @@
 
+$(function(){
+  $('#trigger01').on({
+    'click':function(){
+      $('.nav-list').slideToggle();
+    }
+  })
+})
+
 var swiper = new Swiper(".swiper", {
   pagination: {
     el: ".swiper-pagination",
