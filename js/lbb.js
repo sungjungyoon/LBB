c8e77c306b1c5958ddf12e72ce253722c5c6f35d
sungjungyoon <87048278+sungjungyoon@users.noreply.github.com>
2021-12-06 15:13:41 +0900

    Add files via upload

--- js/lbb.js
+++ js/lbb.js
@@ -4,11 +4,33 @@ $(function(){
     'click':function(){
       $('.nav-list').slideToggle();
     }
+  }),
+  $('.more-btn').on({
+    'click':function(){
+      alert('준비중입니다')
+    }
+  }),
+  $('.link-box').on({
+    'click':function(){
+      alert('준비중입니다')
+    }
   })
 })
+
+
 
 var swiper = new Swiper(".swiper", {
   pagination: {
     el: ".swiper-pagination",
+    type:'bullets',
+    clickable: true,
   },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 3000,
+  },
+  loop:true,
 });
